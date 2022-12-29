b0efa1fbd08f3367f444244b440c25f001d224f0
anvarkhon <anvarkhonmunavvarov@gmail.com>
2022-12-29 19:52:33 +0500

    Initial Commit

---
+++ Guess/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Guess
+//
+//  Created by Anvarkhon Munavvarov on 29/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
